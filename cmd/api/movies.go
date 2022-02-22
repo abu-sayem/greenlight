248485eb48b1de00c73e0a021e26b2b4de9dba00
abu-sayem <abusaayem@gmail.com>
2022-02-22 22:56:09 +0600

    Validate create movie handler with custom validator

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,13 +6,14 @@ import (
 	"time"
 
 	"greenlight.abusayem.net/internal/data"
+	"greenlight.abusayem.net/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
 		Title 	string 		`json:"title"`
 		Year 	int32 		`json:"year"`
-		Runtime int32 		`json:"runtime"`
+		Runtime data.Runtime		`json:"runtime"`
 		Genres 	[]string 	`json:"genres"`
 	}
 
@@ -23,8 +24,24 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	v := validator.New()
+	v.Check(input.Title != "", "title", "must be provided")
+	v.Check(len(input.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(input.Year != 0, "year", "must be provided")
+	v.Check(input.Year >= 1900 && input.Year <= 2100, "year", "must be between 1900 and 2100")
+	v.Check(input.Runtime >= 0, "runtime", "must be greater than 0")
+	v.Check(len(input.Genres) > 0, "genres", "must be provided")
+	v.Check(len(input.Genres) <= 10, "genres", "must not be more than 10 items")
+	v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate values")
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
 	fmt.Fprintf(w, "%+v\n", input)
 }
+
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 
