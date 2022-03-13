323f6b6d15c26ecd74dee4e16066ea0ea6a66f0a
abu-sayem <abusaayem@gmail.com>
2022-03-13 09:25:36 +0600

    Decouple server func from main

---
+++ cmd/api/server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"os/signal"
+	"syscall"
+
+	"net/http"
+	"time"
+)
+
+func (app *application) serve() error {
+	// Declare a HTTP server using the same settings as in our main() function.
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+		app.logger.PrintInfo("caught signal", map[string]string{
+			"signal": s.String(),
+		})
+		os.Exit(0)
+	}()
+
+	app.logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  app.config.env,
+	})
+	return srv.ListenAndServe()
+
+}
+
